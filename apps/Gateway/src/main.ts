ed803af30ed7002481f36abae834697106b9b758
Mehran Mansouri <mehran4496@gmail.com>
2025-12-06 09:38:09 +0100

    feat: setup gateway

--- apps/Gateway/src/main.ts
+++ apps/Gateway/src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import express from 'express';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const server = express();
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
+
+  const configService = app.get(ConfigService);
+  const port = configService.get('PORT');
+  await app.listen(port);
+  console.log(`Server running on port ${port}`);
 }
 bootstrap();
